c15c90c19df16a6c64a769a6d22f844aed321d05
kilianmaigrot <kilian.maigrot@gmail.com>
2024-10-18 14:22:53 +0200

    Modification du getAllJoueurInDb pour rendre un ArrayList<HashMap<String, Object>> itérable

--- Mastermind/src/Modele/Joueur.java
+++ Mastermind/src/Modele/Joueur.java
@@ -1,10 +1,10 @@
 package Modele;
 
 import Utils.DBConnector;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Joueur implements JoueurInterface {
     private int id_joueur;
@@ -71,25 +71,37 @@ public class Joueur implements JoueurInterface {
         }
     }
 
-    public String getAllJoueurInDb() {
+    public ArrayList<HashMap<String, Object>> getAllJoueurInDb() {
         StringBuilder result = new StringBuilder();
         String requete = "SELECT * FROM joueur";
 
         try (
                 Connection con = DBConnector.connectToDB();
                 PreparedStatement stmt = con.prepareStatement(requete);
-                ResultSet rs = stmt.executeQuery()
         ) {
-            while (rs.next()) {
-                int id = rs.getInt("id_joueur");
-                String nom = rs.getString("nom_joueur");
-                result.append("Identifiant: ").append(id).append(", Nom: ").append(nom).append("\n");
-            }
+            ResultSet rs = stmt.executeQuery();
+            return resultSetToList(rs);
         } catch (SQLException e) {
             System.out.println("Erreur DB lors de la récupération des joueurs : " + e.getMessage() + "\n");
             throw new RuntimeException(e);
         }
+    }
 
-        return result.length() > 0 ? result.toString() : "Aucun joueur trouvé.";
+    public ArrayList<HashMap<String, Object>> resultSetToList(ResultSet rs) throws SQLException {
+        ArrayList<HashMap<String, Object>> resultList = new ArrayList<>();
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        while (rs.next()) {
+            HashMap<String, Object> rowMap = new HashMap<>();
+            // Parcourir chaque colonne
+            for (int i = 1; i <= columnCount; i++) {
+                String columnName = metaData.getColumnName(i);
+                Object columnValue = rs.getObject(i);
+                rowMap.put(columnName, columnValue);
+            }
+            resultList.add(rowMap); // Ajouter la ligne à la liste
+        }
+        return resultList;
     }
 }
