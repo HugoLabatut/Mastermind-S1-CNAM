c64e35f1e9df8ff472db75235c6644afb97149e3
kilianmaigrot <kilian.maigrot@gmail.com>
2024-10-23 10:08:12 +0200

    En cours sur ViewJeu / ControllerJeu

--- Mastermind/src/Controllers/JeuController.java
+++ Mastermind/src/Controllers/JeuController.java
@@ -1,10 +1,12 @@
 package Controllers;
 import Enums.Couleur;
+import Modele.Joueur;
+import Modele.Partie;
 import Views.JeuView;
 
 public class JeuController {
-    // private Joueur joueur;
-    // private Partie partie;
+    private Joueur joueur;
+    private Partie partie;
     private JeuView vue;
     private Couleur couleur;
 
---
+++ Mastermind/src/Enums/VuesTypes.java
@@ -0,0 +1,8 @@
+package Enums;
+
+public enum VuesTypes {
+    JOUEURS,
+    PARTIES,
+    JEU
+}
+
